7b5f2caff5cf643790e00f3c2a0c5f18dc5a29dc
Johnny Mayo <johnnymayodev@gmail.com>
2025-05-22 13:30:05 -0400

    Update main.ts

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -7,9 +7,10 @@ export default function create(app: Hono): Hono {
   app.use(
     "/*",
     cors({
-      origin: ["http://localhost:8000", "https://cancion.johnnymayo.com/"],
+      origin: ["http://localhost:8000", "https://cancion.johnnymayo.com"],
     }),
   );
+
   app.route("/api", routes);
 
   app.get("/health", (c) => {
